9782b30867953c6eb264ea4892ffa5012bc8f08b
Francisco Wallison <franciscowallison@gmail.com>
2022-10-19 10:14:23 -0300

    init gr2-web

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ var path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/src')));
+app.use(express.static(path.join(__dirname, '/gr2-web')));
 
 app.get("/",(req, res) =>{
-    console.log("teste")
     res.sendFile(__dirname + "/src/index.html")
 } )
+
+app.get("/gr",(req, res) =>{
+    res.sendFile(__dirname + "/gr2-web/index.html")
+} )
+
+
+app.get("/grep",(req, res) =>{
+    res.sendFile(__dirname + "/gr2-web/index_emperio.html")
+} )
+
 
 app.listen(3333, () => console.log('Server is running! http://localhost:3333'));
